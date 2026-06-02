bddabfd9fb3479c8aa9e61bb2da4e30a91e7612f
dofekrishna4-dev <dofekrishna4@gmail.com>
2026-06-02 16:56:15 +0530

    Day 1: Data ingestion complete

---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+scheme_codes = {
+    "HDFC_Top100": 125497,
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_LargeCap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for fund_name, code in scheme_codes.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    nav_df = pd.DataFrame(data["data"])
+
+    nav_df.to_csv(
+        f"data/raw/{fund_name}_live_nav.csv",
+        index=False
+    )
+
+    print(f"{fund_name} downloaded")
